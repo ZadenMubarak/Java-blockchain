72212cf872fe88d6c500d3b60a6d1eee9d41a084
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-06-19 15:26:31 +0200

    Included spring configuration

---
+++ bigz-blockchain/src/main/java/com/zaden/bigz_blockchain/BigzBlockchainApplication.java
@@ -0,0 +1,13 @@
+package com.zaden.bigz_blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigzBlockchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigzBlockchainApplication.class, args);
+	}
+
+}
---
+++ bigz-blockchain/src/test/java/com/zaden/bigz_blockchain/BigzBlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaden.bigz_blockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigzBlockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
