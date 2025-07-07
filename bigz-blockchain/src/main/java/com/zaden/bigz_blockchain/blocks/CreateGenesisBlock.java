0b5a45f4bdb9bc0653032a565c6a71a0900f7de1
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-07-07 23:19:56 +0200

    fix Makefile

--- bigz-blockchain/src/main/java/com/zaden/bigz_blockchain/blocks/CreateGenesisBlock.java
+++ bigz-blockchain/src/main/java/com/zaden/bigz_blockchain/blocks/CreateGenesisBlock.java
@@ -2,6 +2,9 @@ package com.zaden.bigz_blockchain.blocks;
 
 public class CreateGenesisBlock {
 
+    private String gebesisBlockTestDAta = "This is the very first block"+
+                                            "Because in the beginning etc ...";
+
     public static Block generateGenesisBlock(){
         return new Block();
     }
