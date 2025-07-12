8bd76c727723315a8df3be41367c15c8c0fdcbea
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-07-12 23:32:43 +0200

    fix

--- bigz-blockchain/src/main/java/com/zaden/bigz_blockchain/blocks/CreateGenesisBlock.java
+++ bigz-blockchain/src/main/java/com/zaden/bigz_blockchain/blocks/CreateGenesisBlock.java
@@ -2,6 +2,8 @@ package com.zaden.bigz_blockchain.blocks;
 
 public class CreateGenesisBlock {
 
+    private Block s;
+
     private String gebesisBlockTestDAta = "This is the very first block"+
                                             "Because in the beginning etc ...";
 
